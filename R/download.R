dead2555087361b6213e90633f83dcffd40c8f9c
Felipe Mattioni Maturana <felipemattioni92@gmail.com>
2020-02-04 22:02:51 +0100

    added fontawesome icons to button label

--- R/download.R
+++ R/download.R
@@ -8,6 +8,8 @@
 #' @param output_extension Extension of the output file. Currently, only  \code{.csv} and  \code{.xlsx} are supported.
 #' @param button_label character (HTML), button label
 #' @param button_type character, one of the standard Bootstrap types
+#' @param has_icon specify whether to include fontawesome icons in the button label
+#' @param icon fontawesome tag e.g.: "fa fa-save"
 #' @param ... attributes (named arguments) and children (unnamed arguments)
 #'   of the button, passed to \code{htmltools::\link[htmltools]{tag}}
 #'
@@ -27,13 +29,18 @@ download_this <- function(
   .data,
   output_name,
   output_extension = c(".csv", ".xlsx"),
-  button_label,
+  button_label = "Download data",
   button_type = c("default", "primary", "success", "info", "warning", "danger"),
+  has_icon = TRUE,
+  icon = "fa fa-save",
   ...
 ){
 
   if(!is.data.frame(.data))
     stop("You must pass a data frame to the function.", call. = FALSE)
+
+  ## add fontawesome
+  add_fontawesome()
 
   output_extension <- match.arg(output_extension)
   button_type <- match.arg(button_type)
@@ -48,6 +55,13 @@ download_this <- function(
     readr::write_csv(x = .data, path = tmp_file)
   } else {
     writexl::write_xlsx(x = .data, path = tmp_file)
+  }
+
+  ## create button label
+  if(has_icon) {
+    button_label <- htmltools::HTML(paste(htmltools::tags$i(class = icon), button_label))
+  } else {
+    button_label
   }
 
   ## generate download button
@@ -71,4 +85,11 @@ encode_this <- function(.tmp_file) {
   ## encode file as a base64 string
   ## taken from https://yihui.org/en/2018/07/embed-file/
   base64enc::base64encode(.tmp_file)
+}
+
+add_fontawesome <- function(){
+  ## this will ensure that fontawesome is added
+  ## https://github.com/rstudio/rmarkdown/issues/813
+
+  htmltools::tagList(rmarkdown::html_dependency_font_awesome())
 }
