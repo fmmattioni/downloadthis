62d3e8de09ed34552ced2e4feac01734cec9ce69
runner <runner@Mac-1352.local>
2020-04-21 08:56:34 +0000

    Built site for downloadthis: 0.1.3.9000@f6725f7

---
+++ articles/downloadthis_files/font-awesome-5.13.0/js/script.js
@@ -0,0 +1,1 @@
+document.head.innerHTML += '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />';
