d0d4d3371030c962bc97245b61aea7837e00b555
Felipe Mattioni Maturana <felipemattioni92@gmail.com>
2022-02-04 11:57:01 +0100

    updated tests

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -2,14 +2,14 @@ test_that("data frame works", {
   x <- mtcars %>%
     download_this()
 
-  expect_is(x, "shiny.tag")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("list works", {
   x <- list(mtcars, iris) %>%
     download_this()
 
-  expect_is(x, "shiny.tag")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("ggplot2 works", {
@@ -18,28 +18,28 @@ test_that("ggplot2 works", {
 
   x <- download_this(x)
 
-  expect_is(x, "shiny.tag")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("csv works", {
   x <- mtcars %>%
     download_this(output_extension = ".csv")
 
-  expect_is(x, "shiny.tag")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("xlsx works", {
   x <- mtcars %>%
     download_this(output_extension = ".xlsx")
 
-  expect_is(x, "shiny.tag")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("list xlsx works", {
   x <- list("Dataset 1" = mtcars, "Dataset 2" = iris) %>%
     download_this(output_extension = ".xlsx")
 
-  expect_is(x, "shiny.tag")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("list rds works", {
@@ -49,19 +49,19 @@ test_that("list rds works", {
   x <- list(mtcars, iris, vector_example, linear_model) %>%
     download_this(output_extension = ".rds")
 
-  expect_is(x, "shiny.tag")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("web address works", {
   x <- download_link(link = "https://github.com/fmmattioni/downloadthis/raw/master/inst/example/file_1.pdf")
 
-  expect_is(x, "shiny.tag.list")
+  expect_s3_class(x, "shiny.tag.list")
 })
 
 test_that("local single file works", {
   x <- download_file(path = system.file("assets/css/all.min.css", package = "downloadthis"))
 
-  expect_is(x, "shiny.tag.list")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("local multiple files works", {
@@ -69,11 +69,11 @@ test_that("local multiple files works", {
 
   x <- download_file(path_files, output_name = "Multiple files")
 
-  expect_is(x, "shiny.tag.list")
+  expect_s3_class(x, "shiny.tag")
 })
 
 test_that("local directory works", {
   x <- download_dir(path = system.file("assets", package = "downloadthis"), output_name = "example dir")
 
-  expect_is(x, "shiny.tag.list")
+  expect_s3_class(x, "shiny.tag")
 })
