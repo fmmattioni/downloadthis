c03b97e47fc20506b1d18e228a36ebb5ab2a230d
fmmattioni <36630619+fmmattioni@users.noreply.github.com>
2024-07-04 09:42:25 +0000

    Deploying to gh-pages from @ fmmattioni/downloadthis@0dc2ae974cec3640aa7f8c73dc1aaec1e5fe0578 🚀

---
+++ articles/customization_files/font-awesome-6.5.2/js/script.js
@@ -0,0 +1,6 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css';
+link.type = 'text/css';
+const head = document.getElementsByTagName('head')[0];
+head.appendChild(link);
